ba47011504d1dda08675f04b7bbb59a9e0e042be
wangfanghua618 <wangfanghua618@jk.cn>
2018-09-24 16:25:30 +0800

    处理404页面

--- router/default.js
+++ router/default.js
@@ -1,8 +1,12 @@
 const Router = require('koa-router');
 const router = new Router();
+const fs = require('fs');
+
 
 router.all('*', (ctx, next) => {
-    ctx.body = '404';
+    ctx.type = 'html';
+    ctx.status = 404;
+    ctx.body = fs.createReadStream(process.cwd() + '/public/404.html');
 });
 
 module.exports = router;
