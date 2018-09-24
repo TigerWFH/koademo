77f483184ba9e5bc33f48efdab214d32fe461360
wangfanghua618 <wangfanghua618@jk.cn>
2018-09-24 14:59:00 +0800

    为index添加try……catch语句块

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 const logger = require('koa-logger');
-const Koa  =require('koa');
+const Koa = require('koa');
 const Router = require('koa-router');
+const fs = require('fs');
 
 const app = new Koa();
 const router = new Router();
 
-app.use(logger());
+try {
+    app.use(logger());
+    router.get('/', (ctx, next) => {
+        ctx.body = '跟路径请求';
+    });
 
-router.get('/', (ctx, next) => {
-    ctx.body = '跟路径请求';
-});
+    router.get('/path', (ctx, next) => {
+        ctx.body = 'path';
+    });
 
-router.get('/path', (ctx, next) => {
-    ctx.body = 'path';
-});
+    router.get('/path/123', (ctx, next) => {
+        ctx.body = 'path/123';
+    });
 
-router.get('/path/123', (ctx, next) => {
-    ctx.body = 'path/123';
-});
+    app.use(router.routes()).use(router.allowedMethods());
 
-app.use(router.routes()).use(router.allowedMethods());
+    app.use((res) => {
+        res.body = 'Hello World!';
+    });
 
-app.use((res) => {
-    res.body = 'Hello World!';
-});
+    app.listen(3002);
+    console.log("koa listen on port: 3002")
+}
 
-app.listen(3002);
-console.log("koa listen on port: 3002")
+catch (error) {
+    let errLog = `${error.name},${error.message},${error.stack},${new Date()}`;
+    fs.appendFileSync('./logs/error.log', errLog);
+}
