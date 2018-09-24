1bc40570b9cb142c7e9526128103bb2377dd8e18
wangfanghua618 <wangfanghua618@jk.cn>
2018-09-24 15:48:45 +0800

    修改配置项

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+const fs = require('fs');
 const logger = require('koa-logger');
 const Koa = require('koa');
-const fs = require('fs');
 
 const app = new Koa();
 // 路由
 const index = require('./router');
 const v1  = require('./router/v1');
 
+let config = null;
+if ("development" === process.env.NODE_ENV) {
+    config = require('./config/default.json');
+}
+else {
+    config = require('./config/prod.json');
+}
+
 try {
     app.use(logger());
     // 路由
     app.use(index.routes());
     app.use(v1.routes());
-    
-    app.listen(3002, () => {
-        console.log("koa listen on port: 3002")
+
+    app.listen(config.server.port, () => {
+        console.log(`server is running on ip: ${config.server.host},port:${config.server.port}`)
     });
 }
 catch (error) {
