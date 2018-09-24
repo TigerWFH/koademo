c62a12895384063bee876b3f47a775e68ebba319
wangfanghua618 <wangfanghua618@jk.cn>
2018-09-24 14:31:05 +0800

    初步确立后台目录

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const Logger = require('koa-logger');
+const logger = require('koa-logger');
 const Koa  =require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(Logger((str, args) => {
+app.use(logger());
 
-}))
+router.get('/', (ctx, next) => {
+    ctx.body = '跟路径请求';
+});
+
+router.get('/path', (ctx, next) => {
+    ctx.body = 'path';
+});
+
+router.get('/path/123', (ctx, next) => {
+    ctx.body = 'path/123';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.use((res) => {
     res.body = 'Hello World!';
